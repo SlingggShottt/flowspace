dae4ee8bfb2b6e1f778a6ee4e65e1922a7fa8bfb
SlingggShottt <divyansh.p.m.126@gmail.com>
2026-05-19 20:19:15 +0530

    fix: update RDS postgres version to 16

--- infrastructure/rds.tf
+++ infrastructure/rds.tf
@@ -11,7 +11,7 @@ resource "aws_db_subnet_group" "main" {
 resource "aws_db_instance" "postgres" {
   identifier        = "${var.project_name}-postgres"
   engine            = "postgres"
-  engine_version    = "16.3"
+  engine_version    = "16"
   instance_class    = "db.t3.micro"
   allocated_storage = 20
   storage_type      = "gp2"
